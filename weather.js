239c27ea053fd5eba3ef0afde8fc4fc635cf3767
Abdul Wajihi Khan <164241499+abdulwajihi@users.noreply.github.com>
2024-10-11 22:20:12 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
 const apikey = "f5cd9f462e38ee46209ec3fa390cd5fb";
+
 window.addEventListener("load", () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -6,12 +7,12 @@ window.addEventListener("load", () => {
             let lat = position.coords.latitude;
             const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`;
 
-            fetch(url).then((res) => {
-                return res.json();
-            }).then((data) => {
-                console.log(data);
-                weatherReport(data);
-            });
+            fetch(url)
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    weatherReport(data); // Display current weather
+                });
         });
     }
 });
@@ -20,33 +21,33 @@ function searchByCity() {
     var place = document.getElementById('input').value;
     var urlsearch = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apikey}`;
 
-    fetch(urlsearch).then((res) => {
-        return res.json();
-    }).then((data) => {
-        console.log(data);
-        weatherReport(data);
-    });
-    document.getElementById('input').value = '';
+    fetch(urlsearch)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            weatherReport(data); // Display weather for searched city
+        });
+    document.getElementById('input').value = ''; // Clear input field
 }
 
 function weatherReport(data) {
     var urlcast = `https://api.openweathermap.org/data/2.5/forecast?q=${data.name}&appid=${apikey}`;
 
-    fetch(urlcast).then((res) => {
-        return res.json();
-    }).then((forecast) => {
-        console.log(forecast.city);
-        hourForecast(forecast);
-        dayForecast(forecast);
+    fetch(urlcast)
+        .then((res) => res.json())
+        .then((forecast) => {
+            console.log(forecast.city);
+            hourForecast(forecast); // Display hourly forecast
+            dayForecast(forecast); // Display 4-day forecast
 
-        document.getElementById('city').innerText = data.name + ', ' + data.sys.country;
-        document.getElementById('temperature').innerText = Math.floor(data.main.temp - 273) + ' °C';
-        document.getElementById('clouds').innerText = data.weather[0].description;
+            document.getElementById('city').innerText = data.name + ', ' + data.sys.country;
+            document.getElementById('temperature').innerText = Math.floor(data.main.temp - 273) + ' °C';
+            document.getElementById('clouds').innerText = data.weather[0].description;
 
-        let icon1 = data.weather[0].icon;
-        let iconurl = "https://api.openweathermap.org/img/w/" + icon1 + ".png";
-        document.getElementById('img').src = iconurl;
-    });
+            let icon1 = data.weather[0].icon;
+            let iconurl = "https://api.openweathermap.org/img/w/" + icon1 + ".png";
+            document.getElementById('img').src = iconurl;
+        });
 }
 
 function hourForecast(forecast) {
@@ -79,25 +80,45 @@ function hourForecast(forecast) {
 }
 
 function dayForecast(forecast) {
-    document.querySelector('.weekF').innerHTML = '';
-    for (let i = 8; i < forecast.list.length; i += 8) { // Every 8th index represents the next day's data
+    document.querySelector('.weekF').innerHTML = ''; // Clear previous forecast
+
+    let dayData = {};
+
+    // Aggregate forecast data for each day
+    forecast.list.forEach((entry) => {
+        let date = new Date(entry.dt * 1000).toDateString(undefined, 'Asia/Kolkata');
+
+        if (!dayData[date]) {
+            dayData[date] = {
+                temp_max: entry.main.temp_max,
+                temp_min: entry.main.temp_min,
+                description: entry.weather[0].description
+            };
+        } else {
+            dayData[date].temp_max = Math.max(dayData[date].temp_max, entry.main.temp_max);
+            dayData[date].temp_min = Math.min(dayData[date].temp_min, entry.main.temp_min);
+        }
+    });
+
+    // Extract and display 4 days of forecast
+    Object.keys(dayData).slice(0, 4).forEach((date) => {
         let div = document.createElement('div');
         div.setAttribute('class', 'dayF');
 
         let day = document.createElement('p');
         day.setAttribute('class', 'date');
-        day.innerText = new Date(forecast.list[i].dt * 1000).toDateString(undefined, 'Asia/Kolkata');
+        day.innerText = date;
         div.appendChild(day);
 
         let temp = document.createElement('p');
-        temp.innerText = 'Max: ' + Math.floor(forecast.list[i].main.temp_max - 273) + ' °C / Min: ' + Math.floor(forecast.list[i].main.temp_min - 273) + ' °C';
+        temp.innerText = `Max: ${Math.floor(dayData[date].temp_max - 273)} °C / Min: ${Math.floor(dayData[date].temp_min - 273)} °C`;
         div.appendChild(temp);
 
         let description = document.createElement('p');
         description.setAttribute('class', 'desc');
-        description.innerText = forecast.list[i].weather[0].description;
+        description.innerText = dayData[date].description;
         div.appendChild(description);
 
         document.querySelector('.weekF').appendChild(div);
-    }
+    });
 }
